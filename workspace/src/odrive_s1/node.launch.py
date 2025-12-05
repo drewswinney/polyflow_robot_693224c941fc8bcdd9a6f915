f0b519ec9e339978dd6ef3e326a625134aa6fe3c
Drew Swinney <drewswinney@gmail.com>
2025-12-04 19:33:29 -0500

    Deploy code for PA1

---
+++ workspace/src/odrive_s1/node.launch.py
@@ -0,0 +1,26 @@
+import json
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    parameters = json.loads('{"joint":"6932256d41fc8bcdd9a6f938","control_mode":"position","limit.lower_position":0,"limit.upper_position":360,"limit.position_step":1,"limit.max_effort":10,"limit.effort_step":null,"limit.max_velocity":35,"limit.velocity_step":0}')
+    configuration = json.loads('{"namespace":"/robot/base","rate_hz":150,"lifecycle":true}')
+    inbound_connections = json.loads('[]')
+    outbound_connections = json.loads('[]')
+    env = {
+        "POLYFLOW_NODE_ID": "6932258641fc8bcdd9a6f945",
+        "POLYFLOW_PARAMETERS": json.dumps(parameters),
+        "POLYFLOW_CONFIGURATION": json.dumps(configuration),
+        "POLYFLOW_INBOUND_CONNECTIONS": json.dumps(inbound_connections),
+        "POLYFLOW_OUTBOUND_CONNECTIONS": json.dumps(outbound_connections),
+    }
+
+    return LaunchDescription(
+        [
+            ExecuteProcess(
+                cmd=["python3", "workspace/src/odrive_s1/odrive_s1/node.py"],
+                additional_env=env,
+                output="screen",
+            )
+        ]
+    )
